bf8626213fce12949c2ae9342005fd571339bccf
kizzanaome <naomekizzan@gmail.com>
2023-06-20 17:31:16 +0300

    Feat(HomePage):Fix home page css

--- NomaCakes/src/screens/LandingScreen.js
+++ NomaCakes/src/screens/LandingScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { SafeAreaView, Text, StyleSheet, ImageBackground, View, Image, Button, Alert, TouchableOpacity } from 'react-native';
+import { SafeAreaView, Text, StyleSheet, View, Image, Alert, TouchableOpacity, StatusBar } from 'react-native';
 import { COLORS } from '../constants';
 // import LinearGradient from 'react-native-linear-gradient'
 import { LinearGradient } from 'expo-linear-gradient';
@@ -13,20 +13,22 @@ const LandingScreen = () => {
                 start={{ x: 0, y: 0.5 }}
                 end={{ x: 1, y: 0.5 }}
             >
-                <Image style={styles.logo}
-                    source={require('../assets/images/noma-logo.png')} />
-                <View>
-                    <Image
-                        style={styles.cake}
-                        source={require('../assets/images/cakes/cake.png')}
-                    />
-                    <Text style={styles.text}>Book yourself a cake now and arouse your taste buds for life </Text>
+                <View style={styles.childContainer}>
+                    <Image style={styles.logo}
+                        source={require('../assets/images/noma-logo.png')} />
+                    <View style={styles.contentContainer}>
+                        <Image
+                            style={styles.cakeImage}
+                            source={require('../assets/images/cakes/cake.png')}
+                        />
+                        <Text style={styles.text}>Book yourself a cake now and arouse your taste buds for life </Text>
 
-                    <TouchableOpacity onPress={() => Alert.alert("Signed in succesfully")} style={styles.button} >
-                        <Text style={styles.buttonText}>Sign In</Text>
-                    </TouchableOpacity>
+                        <TouchableOpacity onPress={() => Alert.alert("Signed in succesfully")} style={styles.button} >
+                            <Text style={styles.buttonText}>Sign In</Text>
+                        </TouchableOpacity>
 
-                    <Text style={styles.signupText}>Don't have an account? <Text style={{ color: "#DB4060" }} onPress={() => Alert.alert("Signed up succesfully")}>Sign Up now</Text></Text>
+                        <Text style={styles.signupText}>Don't have an account? <Text style={{ color: "#DB4060" }} onPress={() => Alert.alert("Signed up succesfully")}>Sign Up now</Text></Text>
+                    </View>
                 </View>
             </LinearGradient>
         </SafeAreaView>
@@ -36,64 +38,70 @@ const LandingScreen = () => {
 };
 
 const styles = StyleSheet.create({
+    // mainContainer
     container: {
         flex: 1,
-        backgroundColor: COLORS.lightWhite,
-        position: "absolute",
-        width: 428,
-        height: 926,
-        left: -2,
-        top: 0,
-        borderColor: "red"
+        width: "100%",
+        height: "100%",
         // background: linear - gradient(253.85deg, #DB4060 - 21.73 %, rgba(217, 217, 217, 0) 95.87 %);
         // ImageBackground:
     },
+    // backgroundGradient
     linearGradient: {
         flex: 1,
-        position: 'absolute',
-        top: 0,
-        width: '100%',
-        height: '100%'
+        backgroundColor: COLORS.lightWhite,
+        width: "100%",
+        height: "100%",
+        borderColor: 'green',
+        borderWidth: 2,
+        margin: 0,
+        padding: 0,
+        alignItems: 'center'
     },
+    // holds all the content
+    childContainer: {
+        marginTop: StatusBar.currentHeight,
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+
     logo: {
         width: 232,
         height: 52,
-        left: 60,
-        top: 70,
+        marginTop: 10,
         resizeMode: 'contain'
     },
-    cake: {
-        position: 'absolute',
+    contentContainer: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        textAlign: 'center',
+    },
+    cakeImage: {
         width: 270,
         height: 313,
-        left: 50,
-        top: 100,
+        marginTop: 35,
         resizeMode: 'contain'
     },
     text: {
         width: 300,
-        height: 109,
-        left: 30,
-        top: 390,
-        // borderWidth:1,
-        // borderColor:'red',
         // fontFamily:'Ubuntu'
         fontStyle: 'normal',
         fontWeight: 400,
-        fontSize: 30,
+        fontSize: 27,
         lineHeight: 34,
+        marginTop: -15,
         textAlign: 'center',
         color: "#5B1E3A"
     },
+
+    // button 
     button: {
-        position: 'absolute',
         width: 300,
         height: 64,
-        left: 35,
-        top: 520,
         borderRadius: 50,
         backgroundColor: "#DB4060",
-        padding: 20,
+        justifyContent: 'center',
+        marginTop: 30
     },
     buttonText: {
         color: 'white',
@@ -106,15 +114,13 @@ const styles = StyleSheet.create({
     },
 
     signupText: {
-        position: 'absolute',
         textAlign: 'center',
         width: 245,
         height: 17,
-        left: 70,
-        top: 605,
         fontSize: 15,
         fontWeight: 400,
         fontStyle: 'normal',
+        marginTop: 15,
         lineHeight: 15
     }
 })
